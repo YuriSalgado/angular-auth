64f7329c17fb15025e209d916f2524b43c90782a
unknown <yuri_salgadinh@hotmail.com>
2021-08-01 14:20:54 -0300

    lazy loading in register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
 import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
     NavComponent,
     LoginComponent,
     HomeComponent

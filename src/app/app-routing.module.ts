eb023b13f1331508fb4b2902072add49d5cf66a5
unknown <yuri_salgadinh@hotmail.com>
2021-08-18 20:25:12 -0300

    add lazy loading by module to login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }
 ];
 

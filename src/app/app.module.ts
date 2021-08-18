456a56cd2a3b3670d5837104b39fdd9049f147cb
unknown <yuri_salgadinh@hotmail.com>
2021-08-18 20:35:45 -0300

    add lazy loading by module home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,9 +11,7 @@ import { RegisterService } from './register/register.service';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    LoginComponent,
-    HomeComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
